f7a75e10c0d20a82699336e7f504c41b1f50cfa9
biankavm <bianka.maciel@jusbrasil.com.br>
2026-06-10 22:26:14 -0400

    chore: add eslint ignore rule

--- codebase/react/aula02/BootstrapClient.tsx
+++ codebase/react/aula02/BootstrapClient.tsx
@@ -1,12 +1,13 @@
-"use client";
+'use client'
 
-import { useEffect } from "react";
+import { useEffect } from 'react'
 
 function BootstrapClient() {
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-  return null;
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
+  return null
 }
 
-export default BootstrapClient;
+export default BootstrapClient
