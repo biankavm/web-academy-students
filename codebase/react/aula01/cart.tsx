6bf2319dd3daa7c955a5f8a4a7ab26087a4d7100
biankavm <bianka.maciel@jusbrasil.com.br>
2026-07-12 10:07:50 -0400

    chore: rename /carrinho to /cart

--- codebase/react/aula01/cart.tsx
+++ codebase/react/aula01/cart.tsx
@@ -1,56 +1,55 @@
-"use client";
-import React from "react";
+'use client'
 
 export default function Cart() {
   const getProductTotal = (price: number, quantity: number): number =>
-    price * quantity;
+    price * quantity
 
   return (
     <>
-      <nav className="navbar navbar-expand-md bg-light border-bottom border-body sticky-top">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+      <nav className='navbar navbar-expand-md bg-light border-bottom border-body sticky-top'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='/'>
             WA Loja
           </a>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse"
-            aria-expanded="false"
-            aria-label="Abrir menu"
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarCollapse'
+            aria-controls='navbarCollapse'
+            aria-expanded='false'
+            aria-label='Abrir menu'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <a className="nav-link" href="/">
+          <div className='collapse navbar-collapse' id='navbarCollapse'>
+            <ul className='navbar-nav me-auto mb-2 mb-md-0'>
+              <li className='nav-item'>
+                <a className='nav-link' href='/'>
                   Início
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/carrinho">
+              <li className='nav-item'>
+                <a className='nav-link' href='/cart'>
                   Carrinho
                 </a>
               </li>
             </ul>
 
-            <button className="btn btn-dark">Sair</button>
+            <button className='btn btn-dark'>Sair</button>
           </div>
         </div>
       </nav>
 
       <main>
-        <div className="container p-5">
-          <div className="card mb-4">
-            <div className="row card-body">
-              <h5 className="card-title mb-4 fw-light">
+        <div className='container p-5'>
+          <div className='card mb-4'>
+            <div className='row card-body'>
+              <h5 className='card-title mb-4 fw-light'>
                 Produtos selecionados
               </h5>
-              <div className="table-responsive">
-                <table className="table ">
+              <div className='table-responsive'>
+                <table className='table '>
                   <thead>
                     <tr>
                       <th>Produto</th>
@@ -61,40 +60,40 @@ export default function Cart() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr key="prod-1">
+                    <tr key='prod-1'>
                       <td>Monitor UltraWide 34"</td>
                       <td>R$ {(2200).toFixed(2)}</td>
                       <td>1</td>
 
                       <td>R$ {getProductTotal(2200, 1).toFixed(2)}</td>
                       <td>
-                        <button className="btn btn-danger btn-sm">
+                        <button className='btn btn-danger btn-sm'>
                           Remover
                         </button>
                       </td>
                     </tr>
 
-                    <tr key="prod-2">
+                    <tr key='prod-2'>
                       <td>Teclado Mecânico RGB</td>
                       <td>R$ {(450).toFixed(2)}</td>
                       <td>2</td>
 
                       <td>R$ {getProductTotal(450, 2).toFixed(2)}</td>
                       <td>
-                        <button className="btn btn-danger btn-sm">
+                        <button className='btn btn-danger btn-sm'>
                           Remover
                         </button>
                       </td>
                     </tr>
 
-                    <tr key="prod-3">
+                    <tr key='prod-3'>
                       <td>Mouse Gamer Sem Fio</td>
                       <td>R$ {(350).toFixed(2)}</td>
                       <td>2</td>
 
                       <td>R$ {getProductTotal(350, 2).toFixed(2)}</td>
                       <td>
-                        <button className="btn btn-danger btn-sm">
+                        <button className='btn btn-danger btn-sm'>
                           Remover
                         </button>
                       </td>
@@ -105,11 +104,11 @@ export default function Cart() {
             </div>
           </div>
 
-          <div className="card mb-4">
-            <div className="card-body">
-              <h5 className="card-title mb-4 fw-light">Resumo do Carrinho</h5>
-              <p className="card-text fw-medium">Quantidade total: 5</p>
-              <p className="card-text fw-medium">
+          <div className='card mb-4'>
+            <div className='card-body'>
+              <h5 className='card-title mb-4 fw-light'>Resumo do Carrinho</h5>
+              <p className='card-text fw-medium'>Quantidade total: 5</p>
+              <p className='card-text fw-medium'>
                 Valor total: R${(3800).toFixed(2)}
               </p>
             </div>
@@ -117,5 +116,5 @@ export default function Cart() {
         </div>
       </main>
     </>
-  );
+  )
 }
