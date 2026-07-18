452082638ed32c2b00f603327c5966ac8b9f8189
biankavm <bianka.maciel@jusbrasil.com.br>
2026-07-18 06:27:56 -0400

    refactor: change links ref

--- codebase/react/aula03/login.tsx
+++ codebase/react/aula03/login.tsx
@@ -1,49 +1,49 @@
-"use client";
+'use client'
 
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Login() {
   return (
     <main>
-      <div className="container-fluid d-flex min-vh-100">
-        <div className="row min-vw-100">
-          <div className="col-12 col-md-4 bg-light d-flex justify-content-center align-items-center">
+      <div className='container-fluid d-flex min-vh-100'>
+        <div className='row min-vw-100'>
+          <div className='col-12 col-md-4 bg-light d-flex justify-content-center align-items-center'>
             <h2>Bem vindo à WA Loja!</h2>
-          </div>{" "}
-          <div className="col-12 col-md-8 d-flex justify-content-center align-items-center">
+          </div>{' '}
+          <div className='col-12 col-md-8 d-flex justify-content-center align-items-center'>
             <form>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
+              <div className='mb-3'>
+                <label htmlFor='email' className='form-label'>
                   Email
                 </label>
                 <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="email"
-                  aria-describedby="email"
+                  type='email'
+                  className='form-control form-control-lg'
+                  id='email'
+                  aria-describedby='email'
                   required
                 />
               </div>
-              <div className="mb-3">
-                <label htmlFor="senha" className="form-label">
+              <div className='mb-3'>
+                <label htmlFor='password' className='form-label'>
                   Senha
                 </label>
                 <input
-                  type="password"
-                  className="form-control form-control-lg"
-                  id="senha"
+                  type='password'
+                  className='form-control form-control-lg'
+                  id='password'
                   required
                 />
               </div>
 
-              <div className="d-grid col-12">
-                <button type="submit" className="btn btn-success">
+              <div className='d-grid col-12'>
+                <button type='submit' className='btn btn-success'>
                   Entrar
                 </button>
               </div>
 
-              <div className="text-center mt-3">
-                <Link href="/cadastro" className="btn btn-link">
+              <div className='text-center mt-3'>
+                <Link href='/register' className='btn btn-link'>
                   não tenho cadastro
                 </Link>
               </div>
@@ -52,5 +52,5 @@ export default function Login() {
         </div>
       </div>
     </main>
-  );
+  )
 }
